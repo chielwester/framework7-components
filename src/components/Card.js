855868cdfd5c0360c7d5e387b42b4979d8d753d8
Chris Obdam <>
2020-02-07 15:33:20 -0500

    Add Card component

---
+++ src/components/Card.js
@@ -0,0 +1,22 @@
+(() => ({
+  name: 'Card',
+  type: 'CONTENT',
+  allowedTypes: [],
+  orientation: 'HORIZONTAL',
+  jsx: (() => {
+    const { Card } = window.Framework7ReactComponents;
+    const { env } = B;
+
+    const F7Card = (
+      <Card content="This is a simple card with plain text, but cards can also contain their own header, footer, list view, image, or any other element."></Card>
+    );
+    return env === 'dev' ? (
+      <div className={classes.root}>{F7Card}</div>
+    ) : (
+      F7Card
+    );
+  })(),
+  styles: () => () => ({
+    root: {},
+  }),
+}))();
---
+++ src/prefabs/Card.js
@@ -0,0 +1,19 @@
+(() => ({
+  name: 'Card',
+  icon: 'TitleIcon',
+  category: 'CONTENT',
+  structure: [
+    {
+      name: 'Card',
+      options: [
+        {
+          key: 'content',
+          label: 'Content',
+          value: 'Content',
+          type: 'TEXT',
+        },
+      ],
+      descendants: [],
+    },
+  ],
+}))();
