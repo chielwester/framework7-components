29710b691afa434f57a59816531e91ec2e77a5ce
Chris Obdam <>
2020-02-07 19:20:27 -0500

    Linting

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,18 +4,29 @@
   allowedTypes: [],
   orientation: 'HORIZONTAL',
   jsx: (() => {
-    const { App, View, Page, Navbar, Toolbar, Link } = window.Framework7ReactComponents;
+    const {
+      App,
+      View,
+      Page,
+      Navbar,
+      Toolbar,
+      Link,
+    } = window.Framework7ReactComponents;
     const { env, useText } = B;
 
     const NavBar = (
-      <Navbar title={env === 'dev' ? options.title.join(' ') : useText(options.title)}></Navbar>
-    )
+      <Navbar
+        title={env === 'dev' ? options.title.join(' ') : useText(options.title)}
+      ></Navbar>
+    );
 
-    return (env === 'dev' ? <div className={classes.root}>{NavBar}</div> : NavBar);
+    return env === 'dev' ? (
+      <div className={classes.root}>{NavBar}</div>
+    ) : (
+      NavBar
+    );
   })(),
   styles: B => t => ({
-    root: {
-
-    },
+    root: {},
   }),
 }))();
